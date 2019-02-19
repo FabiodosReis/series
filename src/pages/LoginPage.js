871c173772ce8db01fb0f686f375df6b7e0a0eac
Fabio Reis <fabio_dos_reis@outlook.com>
2019-02-19 15:07:21 -0300

    add pagina de login

---
+++ src/pages/LoginPage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class LoginPage extends React.Component{
+
+
+    render(){
+
+        return (
+            <View>
+                <Text>Login</Text>
+            </View>
+            
+        )
+    }
+}
+
